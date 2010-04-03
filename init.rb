351d3d87f95bbb4ad0b6439e231be13324114195
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-04-03 14:28:38 +0900

    update for redmine 0.9.3

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :redmine_send_schedules do
   name 'Redmine Send Schedules plugin'
   author 'Kazuhiro NISHIYAMA'
   description 'This is a send schedules plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
 end
